9c727896d550396899b8407a949e629284fff845
James Kirby <james.kirby@atlascity.io>
2018-09-28 16:50:11 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,6 @@ Vue.use(VueRouter);
  * If not building with SSR mode, you can
  * directly export the Router instantiation
  */
-
 export default function (/* { store, ssrContext } */) {
   const Router = new VueRouter({
     scrollBehavior: () => ({ y: 0 }),
