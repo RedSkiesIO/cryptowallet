3457ec0f7afe3fd24fc348401b07e8dc360836ff
James Kirkby <jkirkby@protonmail.ch>
2018-10-01 23:18:34 +0100

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,36 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import routes from './routes';
-
 Vue.use(VueRouter);
 
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation
- */
-export default function (/* { store, ssrContext } */) {
-  const Router = new VueRouter({
-    scrollBehavior: () => ({ y: 0 }),
-    routes,
+export default new VueRouter({
+  scrollBehavior: () => ({ y: 0 }),
+  routes: [
+    {
+      path: '/',
+      component: () => import('layouts/UnAuthed.vue'),
+      children: [
+        { path: '', component: () => import('pages/PinPad.vue') },
+      ],
+    },
+    {
+      path: '/balance',
+      component: () => import('layouts/Authed.vue'),
+      children: [
+        { path: '', component: () => import('pages/Index.vue') },
+      ],
+    },
+    {
+      path: '*',
+      component: () => import('layouts/Authed.vue'),
+      children: [
+        { path: '', component: () => import('pages/Error404.vue') },
+      ],
+    },
+  ],
 
-    // Leave these as is and change from quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    mode: process.env.VUE_ROUTER_MODE,
-    base: process.env.VUE_ROUTER_BASE,
-  });
-
-  return Router;
-}
+  // Leave these as is and change from quasar.conf.js instead!
+  // quasar.conf.js -> build -> vueRouterMode
+  mode: process.env.VUE_ROUTER_MODE,
+  base: process.env.VUE_ROUTER_BASE,
+});
