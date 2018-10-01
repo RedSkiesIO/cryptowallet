d415726f5f8b6710f04582571fc68391525155f2
James Kirkby <jkirkby@protonmail.ch>
2018-10-01 23:18:34 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -10,5 +10,20 @@ export default {
 };
 </script>
 
-<style>
+<style lang='scss'>
+
+@import 'bulma/sass/utilities/_all.sass';
+@import 'bulma/sass/base/_all.sass';
+@import 'bulma/sass/layout/_all.sass';
+@import 'bulma/sass/elements/button.sass';
+@import 'bulma/sass/elements/form.sass';
+@import 'bulma/sass/elements/icon.sass';
+@import 'bulma/sass/elements/container.sass';
+@import 'bulma/sass/elements/content.sass';
+@import 'bulma/sass/elements/notification.sass';
+@import 'bulma/sass/elements/title.sass';
+@import 'bulma/sass/components/card.sass';
+@import 'bulma/sass/components/media.sass';
+@import 'bulma/sass/grid/columns.sass';
+
 </style>
