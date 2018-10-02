0591b9494e1037162e01c97799bff5a51920e649
James Kirkby <jkirkby@protonmail.ch>
2018-10-02 11:30:29 +0100

    fix jest quasar loading

---
+++ src/components/RecentPayments/Amount.vue
@@ -0,0 +1,52 @@
+<!--
+
+it renders a div with amount-ox class
+it variant grey is passed, it renders grey-text class
+it rendersp properly formatted amount
+
+ -->
+
+
+<template>
+  <div
+    :class="{ 'grey-text': variant.indexOf('grey') != -1 }"
+    class="amount-box"
+  >
+    <div>{{ formattedAmount }}</div>
+  </div>
+</template>
+
+<script>
+import numeral from 'numeral';
+
+export default {
+  name: 'Amount',
+  props: {
+    amount: {
+      type: Number,
+      required: true,
+    },
+    variant: {
+      type: String,
+      default() {
+        return '';
+      },
+    },
+  },
+  computed: {
+    formattedAmount() {
+      const formatted = `${numeral(this.amount).format('0.00')} £`;
+      return (this.amount > 0 ? `+${formatted}` : formatted);
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .amount-box {
+    display: flex;
+    justify-content: flex-end;
+    text-align: right;
+    white-space: nowrap;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import example from './module-example';
+import wallet from './module-wallet';
 
 Vue.use(Vuex);
 
@@ -9,6 +10,7 @@ export default function (/* { ssrContext } */) {
   const Store = new Vuex.Store({
     modules: {
       example,
+      wallet,
     },
   });
 
