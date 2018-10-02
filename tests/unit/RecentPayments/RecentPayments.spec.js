b852051aa54314642d3372b17c5dbe744b15b0c3
James Kirkby <jkirkby@protonmail.ch>
2018-10-02 11:30:29 +0100

    fix jest quasar loading

---
+++ tests/unit/RecentPayments/RecentPayments.spec.js
@@ -0,0 +1,9 @@
+import { shallowMount } from '@vue/test-utils';
+import RecentPayments from '@/components/RecentPayments/RecentPayments.vue';
+
+describe('RecentPayments.vue', () => {
+  it('tests', () => {
+    console.log(shallowMount);
+    console.log(RecentPayments);
+  });
+});
