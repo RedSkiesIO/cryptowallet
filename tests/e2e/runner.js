199d5cc0464b340aab364e6c1b0bcd1f8cc8f05b
James Kirby <james.kirby@atlascity.io>
2018-10-03 16:00:35 +0000

    Unit test setup

--- tests/e2e/runner.js
+++ tests/e2e/runner.js
@@ -1,40 +1,68 @@
-process.env.NODE_ENV = 'testing';
+process.env.NODE_ENV = 'testing'
 
-// const webpack = require('webpack');
-// const spawn = require('cross-spawn');
-// const QuasarDevServer = require('quasar-cli/lib/dev-server');
-// const QuasarConfigGHenerator = require('quasar-cli/lib/generator');
-// const quasarConfig = require('../../quasar.conf');
-// const webpackConfig = require('../../build/webpack.prod.conf');
-// const devConfigPromise = require('../../build/webpack.dev.conf');
+const webpack = require('webpack')
+const spawn = require('cross-spawn')
+const QuasarDevServer = require('quasar-cli/lib/dev-server')
+const QuasarConfigGenerator = require('quasar-cli/lib/generator')
+const quasarConfig = require('../../quasar.conf')
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+const portfinder = require('portfinder')
 
-// let server;
+const devConfigPromise = new Promise((resolve, reject) => {
+  portfinder.getPort((err, port) => {
+    if (err) {
+      reject(err)
+    } else {
+      const configCompiler = new QuasarConfigGenerator(quasarConfig);
+      // publish the new Port, necessary for e2e tests
+      process.env.PORT = port
+      // add port to devServer config
+      devWebpackConfig.devServer.port = port
 
-// devConfigPromise.then((devConfig) => {
-//   const devServerOptions = devConfig.devServer;
-//   // const compiler = webpack(webpackConfig);
-//   server = new DevServer(quasarConfig);
-//   return server.listen(devServerOptions.port, devServerOptions.host);
-// }).then(() => {
-//   let opts = process.argv.slice(2);
-//   if (opts.indexOf('--config') === -1) {
-//     opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js']);
-//   }
-//   if (opts.indexOf('--env') === -1) {
-//     opts = opts.concat(['--env', 'chrome']);
-//   }
+      // Add FriendlyErrorsPlugin
+      devWebpackConfig.plugins.push(
+        new FriendlyErrorsPlugin({
+          compilationSuccessInfo: {
+            messages: [
+              `Your application is running here: http://${devWebpackConfig.devServer.host}:${port}`
+            ]
+          },
+          onErrors: config.dev.notifyOnErrors
+            ? utils.createNotifierCallback()
+            : undefined
+        })
+      )
 
-//   const runner = spawn('./node_modules/.bin/nightwatch', opts, { stdio: 'inherit' });
+      resolve(devWebpackConfig)
+    }
+  })
+})
 
-//   runner.on('exit', (code) => {
-//     server.close();
-//     process.exit(code);
-//   });
+devConfigPromise.then(devConfig => {
+    const devServerOptions = devConfig.devServer
+    const server = new QuasarDevServer(quasarConfig);
+    return server.listen(devServerOptions.port, devServerOptions.host)
+  })
+  .then(() => {
+    let opts = process.argv.slice(2)
+    if (opts.indexOf('--config') === -1) {
+      opts = opts.concat(['--config', 'test/e2e/nightwatch.conf.js'])
+    }
+    if (opts.indexOf('--env') === -1) {
+      opts = opts.concat(['--env', 'chrome'])
+    }
 
-//   runner.on('error', (err) => {
-//     server.close();
-//     throw err;
-//   });
-// });
+    const runner = spawn('./node_modules/.bin/nightwatch', opts, {
+      stdio: 'inherit'
+    })
 
-// const server = new QuasarDevServer(quasarConfig);
+    runner.on('exit', code => {
+      server.close()
+      process.exit(code)
+    })
+
+    runner.on('error', err => {
+      server.close()
+      throw err
+    })
+  })
