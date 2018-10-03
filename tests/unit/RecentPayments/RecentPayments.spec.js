c3bd03219ada5da0447da2ea3de3821bec2fd2d0
James Kirkby <james.kirby@atlascity.io>
2018-10-03 10:38:38 +0100

    hotfix bugs

--- tests/unit/RecentPayments/RecentPayments.spec.js
+++ tests/unit/RecentPayments/RecentPayments.spec.js
@@ -1,9 +1,48 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
 import RecentPayments from '@/components/RecentPayments/RecentPayments.vue';
+import Vuex from 'vuex';
+
+import { __createMocks as createStoreMocks } from '../../../src/store/__mocks__/store.js';
+const localVue = createLocalVue();
+localVue.use(Vuex);
 
 describe('RecentPayments.vue', () => {
+  let storeMocks;
+  let wrapper;
+  let store;
+
+  function wrapperInit (options) {
+    return shallowMount(RecentPayments, options);
+  }
+
+  function storeInit (custom) {
+    storeMocks = createStoreMocks(custom);
+    wrapper = wrapperInit({ localVue, store: storeMocks.store });
+    store = wrapper.vm.$store;
+  }
+
+  beforeEach(() => storeInit());
+
   it('tests', () => {
-    console.log(shallowMount);
-    console.log(RecentPayments);
+
   });
+
+/*
+  const propsData = {
+
+  };
+
+  const stubs = {
+    ToDoItem: '<li></li>',
+  };
+
+  function wrapperInit (options) {
+    return shallowMount(Main, options);
+  }
+
+  it('renders and matches snapshot', () => {
+
+  });
+*/
+
 });
