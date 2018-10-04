303360dde819cc087d02de21e88603f428e21fae
James kirby <nshcore@protonmail.com>
2018-10-04 07:54:56 +0000

    E2e runner setup

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,9 +1,14 @@
-module.exports = {
-  'default e2e tests': (browser) => {
-    browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.sidenav')
-      .end();
-  },
-};
+const fs = require('fs');
+
+describe('Screenshots', () => {
+  it('shows text is visible', () => {
+    browser.timeouts('implicit', 5000);
+
+    // We need to switch to the native context for the screenshot to work
+    browser.context('NATIVE_APP');
+
+    // browser.screenshot returns the screenshot as a base64 string
+    const textVisibleScreenshot = browser.screenshot();
+    fs.writeFileSync('screenshot-1.png', textVisibleScreenshot.value, 'base64');
+  })
+});
