5e4a2525ea100c765785449db400014047e57c73
Konrad moskal <konrad.moskal@atlascity.io>
2018-10-04 14:01:11 +0000

    Resolve "As a user I want to see a list of recent payments"

--- src/components/RecentPayments/RecentPayments.vue
+++ src/components/RecentPayments/RecentPayments.vue
@@ -2,45 +2,59 @@
   <section class="recent-payments">
     <PaymentsGroup
       v-for="group in groupedPayments"
-      :key="group"
+      :key="group.key"
       :group="group"
     />
   </section>
 </template>
 
 <script>
-import { date } from 'quasar';
 import PaymentsGroup from './PaymentsGroup.vue';
+
+const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 export default {
   name: 'RecentPayments',
   components: {
     PaymentsGroup,
   },
-
   computed: {
     payments() {
       return this.$store.state.wallet.payments;
     },
+    /*
+     * raw payments data is a flat array, it has to be transformed first,
+     * payments have to be grouped by date, the function returns a groupedPayments Array
+     * each group in the Array represents a day, eg. 12 November
+     * each group will include payments that happened on that day
+     * @return {Array}
+     */
     groupedPayments() {
-      console.log(date);
       const sortedByDate = this.payments.slice().sort((a, b) => b.ts - a.ts);
       const groupedPayments = [];
+      let groupKey = 0;
 
       sortedByDate.forEach((payment) => {
-        let timeStamp = new Date(payment.ts * 1000);
-        timeStamp = `${timeStamp.getDay} ${timeStamp.getYear}`;
-        const groupItem = groupedPayments.find(item => item.date === timeStamp);
+        const day = new Date(payment.ts).getDate();
+        const month = monthNames[new Date(payment.ts).getMonth()];
+        const paymentDate = `${day} ${month}`;
+        // eslint-disable-next-line
+        const groupItem = groupedPayments.find((item) => {
+          if (item) return item.date === paymentDate;
+        });
 
         if (!groupItem) {
           groupedPayments.push({
-            date: timeStamp,
+            key: groupKey,
+            date: paymentDate,
             payments: [],
           });
+
+          groupKey += 1;
         }
 
         groupedPayments.map((item) => {
-          if (item.date === timeStamp) item.payments.push(payment);
+          if (item.date === paymentDate) item.payments.push(payment);
           return item;
         });
       });
