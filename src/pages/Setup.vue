f91fe770e6e126f4286aba67ec7bc52392b6a187
James Kirkby <james.kirby@atlascity.io>
2018-10-05 15:38:13 +0100

    generate user a bip39 seed and store salt in vuex

--- src/pages/Setup.vue
+++ src/pages/Setup.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>hey im a setup</div>
+  <section>
+    <div>
+      <h1>CryptoWallet Setup!</h1>
+      <span>This is your seed for the love of satoshi write it down!</span>
+      <div>{{ seed }}</div>
+    </div>
+  </section>
 </template>
 
 <script>
+import bip39 from 'bip39';
+import bcrypt from 'bcryptjs';
 
 export default {
 
   data() {
     return {
-
+      seed: null,
     };
   },
 
@@ -21,7 +29,10 @@ export default {
   },
 
   beforeMount() {
-
+    this.seed = bip39.generateMnemonic();
+    console.log(this);
+    console.log(bcrypt.genSaltSync(42));
+    this.$store.dispatch('wallet/setSalt', bcrypt.genSaltSync(42));
   },
 
   mounted() {
--- src/store/wallet/mutations.js
+++ src/store/wallet/mutations.js
@@ -1,4 +1,8 @@
-/*
-export function someMutation (state) {
+/**
+ * State mutation for salt
+ * @param {*} state
+ */
+export function SET_SALT(state, salt) {
+  state.salt = salt;
 }
-*/
+
