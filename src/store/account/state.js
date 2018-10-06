912cdbf3405ace40c2ae7dfb62d99d2fece679f7
James Kirkby <jkirkby@protonmail.ch>
2018-10-06 17:22:52 +0100

    update

---
+++ src/store/account/state.js
@@ -0,0 +1,4 @@
+export default {
+  salt: null,
+  pin: null,
+};
--- src/store/wallet/getters.js
+++ src/store/wallet/getters.js
@@ -1,7 +1,4 @@
-/**
- * return the store salt
- * @param {*} state
- */
-export function getSalty(state) {
-  return state.salt;
+/*
+export function someGetter (state) {
 }
+*/
