a72002c6b6527f5a02a710c61f2d29559582f247
Konrad <konrad.moskal@atlascity.io>
2018-10-08 09:14:58 +0100

    commiting before develop pull

--- src/pages/Balance.vue
+++ src/pages/Balance.vue
@@ -1,19 +1,24 @@
 <template>
-  <div>
+  <q-layout>
     <UserBalance/>
     <RecentPayments/>
-  </div>
+    <q-layout-footer>
+      <MainNav/>
+    </q-layout-footer>
+  </q-layout>
 </template>
 
 <script>
 import UserBalance from '../components/UserBalance/UserBalance.vue';
 import RecentPayments from '../components/RecentPayments/RecentPayments.vue';
+import MainNav from '../components/MainNav/MainNav.vue';
 
 export default {
   name: 'Balance',
   components: {
     UserBalance,
     RecentPayments,
+    MainNav,
   },
   data() {
     return {
