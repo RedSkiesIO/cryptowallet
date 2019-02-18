8d53f4fd0c69d504c570f85219d1d65b1c6719b3
James Kirkby <james.kirby@atlascity.io>
2019-02-18 19:01:39 +0000

    fixing eslint deps 💪 💪 💪 💪 💪 💪

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,5 +35,6 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
     'arrow-body-style': ['error', 'always'],
+    'vue/component-name-in-template-casing': ["error", "PascalCase"]
   },
 };
