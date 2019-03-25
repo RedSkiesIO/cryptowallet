bf378cb20587783cd34851925a50bd22a2d44298
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-03-25 16:32:20 +0000

    update

--- test/jest/__tests__/pages/Setup/Steps/Complete.spec.js
+++ test/jest/__tests__/pages/Setup/Steps/Complete.spec.js
@@ -1,32 +1,34 @@
-// // import { mount } from '@vue/test-utils';
-// // import Complete from '@/pages/Setup/Steps/Complete';
-// import { localVue } from '@/helpers/SetupLocalVue';
+// import { mount, createWrapper } from '@vue/test-utils';
+// import Complete from '@/pages/Setup/Steps/Complete';
+// import { localVue, i18n } from '@/helpers/SetupLocalVue';
 // import Vuex from 'vuex';
 // import { Quasar, uid } from 'quasar';
 // import Account from '@/store/wallet/entities/account';
-// // import Wallet from '@/store/wallet/entities/wallet';
+// import Wallet from '@/store/wallet/entities/wallet';
+// import supportedCoins from '@/store/settings/state/supportedCoins.js';
 
 // describe('Complete.vue', () => {
-//   // let store;
+//   let store;
 //   let getters;
 //   let actions;
-//   // let wrapper;
+//   let wrapper;
 
 //   localVue.use(Quasar, { components: { uid } });
-//   localVue.use(Account);
+//   //   localVue.use(Account);
+//   //   localVue.use(Wallet);
 
 //   beforeEach(() => {
-//     const mockGet = [
-//       {
-//         name: 'Stephen',
-//       },
-//     ];
-//     const mockAccounts = {
-//       get: () => { return mockGet; },
-//     };
-//     getters = {
-//       'entities/account/query': () => { return () => { return mockAccounts; }; },
-//     };
+//     // const mockGet = [
+//     //   {
+//     //     name: 'Stephen',
+//     //   },
+//     // ];
+//     // const mockAccounts = {
+//     //   get: () => { return mockGet; },
+//     // };
+//     // getters = {
+//     //   'entities/account/query': () => { return () => { return mockAccounts; }; },
+//     // };
 //     actions = {
 //       'settings/setLoading': jest.fn(),
 //       'settings/setSelectedAccount': jest.fn(),
@@ -34,14 +36,22 @@
 //     store = new Vuex.Store({
 //       state: {
 //         settings: {
+//           supportedCoins,
 //           delay: 500,
+//         },
+//         setup: {
+
 //         },
 //       },
 //       actions,
-//       getters,
+//       // getters,
 //     });
-//     // wrapper = mount(Complete, {
-//     // i18n, localVue, store,
-//     // });
+//     wrapper = mount(Complete, {
+//       i18n, localVue, store,
+//     });
+//   });
+
+//   it('renders and matches snapshot', () => {
+//     expect(wrapper.element).toMatchSnapshot();
 //   });
 // });
