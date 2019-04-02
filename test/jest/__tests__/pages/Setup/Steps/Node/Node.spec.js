76411b23219628a329fd213fbce80dbf04a7d2cd
konradmoskal <konrad.moskal@atlascity.io>
2019-04-02 11:47:10 +0100

    update

--- test/jest/__tests__/pages/Setup/Steps/Node/Node.spec.js
+++ test/jest/__tests__/pages/Setup/Steps/Node/Node.spec.js
@@ -1,10 +1,9 @@
-/*eslint-disable*/
 import { mount } from '@vue/test-utils';
-import Splash from '@/pages/Setup/Steps/Splash';
+import Node from '@/pages/Setup/Steps/Node';
 import { localVue, i18n, createRouter } from '@/helpers/SetupLocalVue';
 import { createMocks as createStoreMocks } from '@/store/__mocks__/store.js';
 
-describe('Splash Setup', () => {
+describe('Node Setup', () => {
   let wrapper;
   let router;
   let store;
@@ -13,7 +12,7 @@ describe('Splash Setup', () => {
   const propsData = {};
 
   function wrapperInit(options) {
-    return mount(Splash, options);
+    return mount(Node, options);
   }
 
   function storeInit(custom) {
@@ -36,4 +35,31 @@ describe('Splash Setup', () => {
     expect(wrapper.element).toMatchSnapshot();
   });
 
+  it('skips to the next setup page if "Skip" button is clicked', () => {
+    const page = parseInt(store.state.route.params.id, 10);
+    wrapper.findAll('.q-btn').at(0).trigger('click');
+    expect(parseInt(store.state.route.params.id, 10)).toBe(page + 1);
+  });
+
+  describe('"Done" button', () => {
+    it('validates the IP Address, if invalid displays toast', () => {
+      const input = wrapper.find('input');
+      input.element.value = '123.a.123';
+      input.trigger('input');
+
+      wrapper.vm.$toast.create = jest.fn();
+      wrapper.findAll('.q-btn').at(1).trigger('click');
+      expect(wrapper.vm.$toast.create).toHaveBeenCalled();
+    });
+
+    it('validates the IP Address, if valid goes to the nest setup page', () => {
+      const input = wrapper.find('input');
+      input.element.value = '123.123.123.123';
+      input.trigger('input');
+
+      const page = parseInt(store.state.route.params.id, 10);
+      wrapper.findAll('.q-btn').at(1).trigger('click');
+      expect(parseInt(store.state.route.params.id, 10)).toBe(page + 1);
+    });
+  });
 });
