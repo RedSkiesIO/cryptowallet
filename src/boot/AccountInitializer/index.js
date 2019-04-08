f66b0050037a31d511e70353200991e0d6eccf57
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-04-08 14:34:35 +0100

    add accountInitializer unit tests

--- src/boot/AccountInitializer/index.js
+++ src/boot/AccountInitializer/index.js
@@ -33,8 +33,8 @@ const accountInitializer = {
     const password = setup.pinArray.join('');
     const SDK = new CryptoWalletSDK();
     const promises = [];
-
     coins.forEach((coin) => {
+      const coinSDK = SDK.SDKFactory.createSDK(coin.sdk);
       const wallet = {
         account_id: id,
         name: coin.name,
@@ -43,10 +43,8 @@ const accountInitializer = {
         sdk: coin.sdk,
         network: coin.network,
       };
-
       if (coin.sdk !== 'ERC20') {
         promises.push(new Promise(async (resolve) => {
-          const coinSDK = SDK.SDKFactory.createSDK(coin.sdk);
           wallet.hdWallet = await coinSDK.generateHDWallet(
             Object.values(setup.seed).join(' ').trim(),
             coin.network,
@@ -56,8 +54,8 @@ const accountInitializer = {
         }));
       }
     });
-
     await Promise.all(promises);
+    return true;
   },
 
   async createERC20Wallets(setup, id, coins) {
