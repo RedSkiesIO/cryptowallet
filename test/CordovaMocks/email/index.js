4d3731635072d80f5d9dc55041f733f33786b880
dufia <konrad.moskal@atlascity.io>
2019-04-08 15:44:16 +0100

    fixing codacy issues

--- test/CordovaMocks/email/index.js
+++ test/CordovaMocks/email/index.js
@@ -1,13 +1,13 @@
 function Email() {
   this.hasPermission = (callback) => { callback(); };
   this.requestPermissionBehaviour = null;
-  this.requestPermission = function (callback) {
+  this.requestPermission = function requestPermission(callback) {
     if (!this.requestPermissionBehaviour) { throw new Error('mock behaviour not specified'); }
     if (this.requestPermissionBehaviour === 'grant') { callback(true); }
     if (this.requestPermissionBehaviour === 'deny') { callback(false); }
   };
   this.isAvailableBehaviour = null;
-  this.isAvailable = function (callback) {
+  this.isAvailable = function isAvailable(callback) {
     if (!this.isAvailableBehaviour) { throw new Error('mock behaviour not specified'); }
     if (this.isAvailableBehaviour === 'yes') { callback(true); }
     if (this.isAvailableBehaviour === 'no') { callback(false); }
