ba8751d4303427765b3ba356445146184f96e9ba
James kirby <nshcore@protonmail.com>
2019-04-10 16:29:20 +0100

    Add failed to send transaction modal (#248)
    * added the send-failed modal

--- src/store/modals/__mocks__/modals.js
+++ src/store/modals/__mocks__/modals.js
@@ -2,12 +2,18 @@ const actions = {
   setSendCoinModalOpened: jest.fn(),
   setConfirmSendModalOpened: jest.fn(),
   setConfirmTransactionData: jest.fn(),
+  setSendFailureModalOpened: jest.fn(),
+  setSendSuccessModalOpened: jest.fn(),
 };
 
 const getters = {};
 const mutations = {};
 const state = {
   sendCoinModalOpened: false,
+  sendConfirmModalOpened: false,
+  sendConfirmTxData: null,
+  sendFailureModalOpened: false,
+  sendSuccessModalOpened: false,
 };
 
 const qrcode = {
