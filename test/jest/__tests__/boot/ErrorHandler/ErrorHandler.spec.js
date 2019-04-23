86a052c527829f589a06ffbd961e4ac5b2221bed
Stephen <stephen.horsfall@atlascity.io>
2019-04-23 14:06:55 +0100

    add error handler plugin unit tests

---
+++ test/jest/__tests__/boot/ErrorHandler/ErrorHandler.spec.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-console */
+import ErrorHandler from '@/boot/ErrorHandler/';
+import { shallowMount } from '@vue/test-utils';
+import { localVue } from '@/helpers/SetupLocalVue';
+import { createMocks as createStoreMocks } from '@/store/__mocks__/store.js';
+import Account from '@/store/wallet/entities/account';
+
+describe('boot/ErrorHandler', () => {
+  let wrapperMock;
+  let storeMocks;
+
+  const mockAccount = {
+    id: 1,
+    name: 'Stephen',
+    locale: 'en-gb',
+  };
+
+  const defaultProps = {};
+
+  function wrapperInit(options) {
+    return shallowMount({ name: 'mock', template: '<div/>' }, options);
+  }
+
+  function storeInit(custom, propsData) {
+    storeMocks = createStoreMocks(custom);
+    ErrorHandler({ Vue: localVue, store: storeMocks.store });
+    localVue.rollbar.configure = jest.fn();
+    localVue.rollbar.error = jest.fn();
+    wrapperMock = wrapperInit({
+      localVue,
+      propsData,
+      store: storeMocks.store,
+    });
+  }
+
+  beforeEach(() => { storeInit({}, defaultProps); });
+
+  it('exports a function', () => {
+    expect(typeof ErrorHandler).toBe('function');
+  });
+
+  it('adds errorHandler to vue prototype', () => {
+    expect(wrapperMock.vm.errorHandler).toBeTruthy();
+  });
+
+  it('sends error to rollbar when errorHandler is called', () => {
+    Account.insert({ data: mockAccount });
+    console.error = jest.fn();
+    wrapperMock.vm.$toast.create = jest.fn();
+    wrapperMock.vm.errorHandler(new Error('Some Error'));
+    expect(localVue.rollbar.configure).toHaveBeenCalledWith({ payload: { account: { name: 'Stephen' } } });
+    expect(localVue.rollbar.error).toHaveBeenCalledWith('Some Error');
+    expect(console.error).toHaveBeenCalled();
+  });
+
+  it('no toast is displayed if showToast is false', () => {
+    console.error = jest.fn();
+    wrapperMock.vm.$toast.create = jest.fn();
+    wrapperMock.vm.errorHandler(new Error('Some Error'), false);
+    expect(wrapperMock.vm.$toast.create).toHaveBeenCalledTimes(0);
+    expect(console.error).toHaveBeenCalledTimes(0);
+  });
+});
