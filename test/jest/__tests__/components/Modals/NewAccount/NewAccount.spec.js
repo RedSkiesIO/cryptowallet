af2741583e9dc3d0d9fe01f4a5f752709350ae64
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-05-01 14:04:37 +0100

    add selectAccount and newAccount modals unit tests

---
+++ test/jest/__tests__/components/Modals/NewAccount/NewAccount.spec.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-magic-numbers */
+import { mount } from '@vue/test-utils';
+import NewAccount from '@/components/Modals/NewAccount/NewAccountContent';
+import { localVue, i18n, createRouter } from '@/helpers/SetupLocalVue';
+import { createMocks as createStoreMocks } from '@/store/__mocks__/store.js';
+
+describe('GetStartedModalContent component', () => {
+  let wrapper;
+  let router;
+  let storeMocks;
+
+  const defaultProps = {};
+
+  function wrapperInit(options) {
+    return mount(NewAccount, options);
+  }
+
+  function storeInit(custom, propsData) {
+    storeMocks = createStoreMocks(custom);
+    router = createRouter(storeMocks.store);
+    router.push({ path: '/' });
+    wrapper = wrapperInit({
+      i18n,
+      router,
+      localVue,
+      propsData,
+      store: storeMocks.store,
+    });
+  }
+
+  beforeEach(() => { storeInit({}, defaultProps); });
+
+  it('renders and matches snapshot', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('renders create account and import account buttons', () => {
+    expect(wrapper.findAll('button').at(1).text()).toMatch('Create Account');
+    expect(wrapper.findAll('button').at(2).text()).toMatch('Import Account');
+  });
+
+  it('back button closes the modal', () => {
+    wrapper.findAll('button').at(0).trigger('click');
+    expect(storeMocks.actions.setNewAccountModalOpened).toHaveBeenCalled();
+  });
+
+  it('create account button closes modals and routes /setup/2', () => {
+    wrapper.findAll('button').at(1).trigger('click');
+    expect(storeMocks.actions.setNewAccountModalOpened).toHaveBeenCalled();
+    expect(storeMocks.actions.setSelectAccountModalOpened).toHaveBeenCalled();
+    expect(storeMocks.actions.setAccountType.mock.calls[0][1]).toBe('new');
+    expect(router.history.current.path).toBe('/setup/2');
+  });
+
+  it('import account button closes modals and routes /setup/1', () => {
+    wrapper.findAll('button').at(2).trigger('click');
+    expect(storeMocks.actions.setNewAccountModalOpened).toHaveBeenCalled();
+    expect(storeMocks.actions.setSelectAccountModalOpened).toHaveBeenCalled();
+    expect(storeMocks.actions.setAccountType.mock.calls[1][1]).toBe('restored');
+    expect(router.history.current.path).toBe('/setup/1');
+  });
+});
---
+++ test/jest/__tests__/components/Modals/SelectAccount/SelectAccount.spec.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-magic-numbers */
+import { mount } from '@vue/test-utils';
+import SelectAccount from '@/components/Modals/SelectAccount/SelectAccountContent';
+import { localVue, i18n, createRouter } from '@/helpers/SetupLocalVue';
+import { createMocks as createStoreMocks } from '@/store/__mocks__/store.js';
+import Account from '@/store/wallet/entities/account.js';
+
+describe('GetStartedModalContent component', () => {
+  let wrapper;
+  let router;
+  let storeMocks;
+
+  const mockAccounts = [{ name: 'Account1', default: true }, { name: 'Account2', default: false }, { name: 'Account3', default: false }];
+  const defaultProps = {};
+
+  function wrapperInit(options) {
+    return mount(SelectAccount, options);
+  }
+
+  function storeInit(custom, propsData) {
+    storeMocks = createStoreMocks(custom);
+    Account.insert({ data: mockAccounts });
+    router = createRouter(storeMocks.store);
+    router.push({ path: '/' });
+    wrapper = wrapperInit({
+      i18n,
+      router,
+      localVue,
+      propsData,
+      store: storeMocks.store,
+    });
+  }
+
+  beforeEach(() => { storeInit({}, defaultProps); });
+
+  it('renders and matches snapshot', () => {
+    expect(wrapper.element).toMatchSnapshot();
+  });
+
+  it('renders three account items', () => {
+    expect(wrapper.findAll('.account-item').length).toBe(3);
+  });
+
+  it('closes the modal when the back button is clicked', () => {
+    wrapper.findAll('button').at(0).trigger('click');
+    expect(storeMocks.actions.setSelectAccountModalOpened.mock.calls[0][1]).toBe(false);
+  });
+
+  it('opens the new account modal when the new account icon is clicked', () => {
+    wrapper.findAll('button').at(1).trigger('click');
+    expect(storeMocks.actions.setNewAccountModalOpened.mock.calls[0][1]).toBe(true);
+  });
+
+  it('changes selected account on account-item click', () => {
+    wrapper.findAll('.account-item').at(2).trigger('click');
+    expect(storeMocks.actions.setSelectedAccount.mock.calls[0][1]).toBe('Account3');
+    wrapper.findAll('.account-item').at(0).trigger('click');
+    expect(storeMocks.actions.setSelectedAccount.mock.calls[1][1]).toBe('Account1');
+  });
+
+  it('changes default account on account-item default toggle', (done) => {
+    Account.$update = Account.update;
+    expect(Account.all()[0].default).toBe(true);
+    expect(Account.all()[1].default).toBe(false);
+    wrapper.findAll('.q-toggle').at(1).trigger('click');
+    expect(Account.all()[1].default).toBe(true);
+    expect(Account.all()[0].default).toBe(false);
+    setTimeout(() => {
+      wrapper.findAll('.q-toggle__inner').at(1).trigger('click');
+      done();
+    }, 0);
+  });
+});
