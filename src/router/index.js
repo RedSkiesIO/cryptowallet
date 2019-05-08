c7cc0c0010b2107d4a952bddb2ea05c6efe56dbb
Stephen <stephen.horsfall@atlascity.io>
2019-05-08 09:28:29 +0100

    244 as a developer 100% coverage on src/router (#284)
    * add refreshWallet unit tests
    
    * add router unit tests

--- src/router/index.js
+++ src/router/index.js
@@ -13,100 +13,100 @@ export default new VueRouter({
   routes: [
     {
       path: '/',
-      component: () => { return import('layouts/UnAuthed'); },
+      component: () => { return import('@/layouts/UnAuthed'); },
       children: [
-        { path: '', component: () => { return import('pages/Auth/Login'); } },
+        { path: '', component: () => { return import('@/pages/Auth/Login'); } },
       ],
     },
     {
       path: '/setup/:id',
-      component: () => { return import('layouts/UnAuthed'); },
+      component: () => { return import('@/layouts/UnAuthed'); },
       children: [
-        { name: 'setup', path: '', component: () => { return import('pages/Setup'); } },
+        { name: 'setup', path: '', component: () => { return import('@/pages/Setup'); } },
       ],
     },
     {
       path: '/settings',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'settings', path: '', component: () => { return import('pages/Settings'); } },
+        { name: 'settings', path: '', component: () => { return import('@/pages/Settings'); } },
       ],
     },
     {
       path: '/wallet',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'wallet', path: '', component: () => { return import('pages/Wallet'); } },
+        { name: 'wallet', path: '', component: () => { return import('@/pages/Wallet'); } },
       ],
     },
     {
       path: '/wallet/single/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'walletSingle', path: '', component: () => { return import('pages/WalletSingle'); } },
+        { name: 'walletSingle', path: '', component: () => { return import('@/pages/WalletSingle'); } },
       ],
     },
     {
       path: '/wallet/single/prices/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'coinSinglePrices', path: '', component: () => { return import('pages/WalletSingle'); } },
+        { name: 'coinSinglePrices', path: '', component: () => { return import('@/pages/WalletSingle'); } },
       ],
     },
     {
       path: '/wallet/single/send/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'sendCoinSingle', path: '', component: () => { return import('pages/WalletSingle'); } },
+        { name: 'sendCoinSingle', path: '', component: () => { return import('@/pages/WalletSingle'); } },
       ],
     },
     {
       path: '/wallet/send/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'sendCoin', path: '', component: () => { return import('pages/Wallet'); } },
+        { name: 'sendCoin', path: '', component: () => { return import('@/pages/Wallet'); } },
       ],
     },
     {
       path: '/wallet/single/receive/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'receiveCoinSingle', path: '', component: () => { return import('pages/WalletSingle'); } },
+        { name: 'receiveCoinSingle', path: '', component: () => { return import('@/pages/WalletSingle'); } },
       ],
     },
     {
       path: '/wallet/receive/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'receiveCoin', path: '', component: () => { return import('pages/Wallet'); } },
+        { name: 'receiveCoin', path: '', component: () => { return import('@/pages/Wallet'); } },
       ],
     },
     {
       path: '/wallet/history/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'coinHistory', path: '', component: () => { return import('pages/Wallet'); } },
+        { name: 'coinHistory', path: '', component: () => { return import('@/pages/Wallet'); } },
       ],
     },
     {
       path: '/wallet/prices/:id',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'coinPrices', path: '', component: () => { return import('pages/Wallet'); } },
+        { name: 'coinPrices', path: '', component: () => { return import('@/pages/Wallet'); } },
       ],
     },
     {
       path: '/wallet/exchange',
-      component: () => { return import('layouts/Authed'); },
+      component: () => { return import('@/layouts/Authed'); },
       children: [
-        { name: 'exchange', path: '', component: () => { return import('pages/Exchange'); } },
+        { name: 'exchange', path: '', component: () => { return import('@/pages/Exchange'); } },
       ],
     },
     {
       path: '*',
-      component: () => { return import('layouts/UnAuthed'); },
+      component: () => { return import('@/layouts/UnAuthed'); },
       children: [
-        { path: '', component: () => { return import('pages/Error/404'); } },
+        { name: '404', path: '', component: () => { return import('@/pages/Error/404'); } },
       ],
     },
   ],
