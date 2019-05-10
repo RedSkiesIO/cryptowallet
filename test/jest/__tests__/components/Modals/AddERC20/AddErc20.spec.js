282743880cf3055737901c67070e1638822e54be
Stephen <stephen.horsfall@atlascity.io>
2019-05-10 12:41:31 +0100

    246 as a developer i want 100% test coverage on src/store/setup (#287)
    * add vuex setup module unit tests
    
    * add vuex settings module unit tests
    
    * add  vuex modal and qrcode modules unit tests
    
    * add receive coin modal test
    
    * remove unused store account module
    
    * add parent modal tests to modal child components

--- test/jest/__tests__/components/Modals/AddERC20/AddErc20.spec.js
+++ test/jest/__tests__/components/Modals/AddERC20/AddErc20.spec.js
@@ -1,11 +1,13 @@
 /* eslint-disable no-magic-numbers */
 import { mount } from '@vue/test-utils';
 import AddErc20 from '@/components/Modals/AddErc20/AddErc20Content.vue';
+import AddErc20Modal from '@/components/Modals/AddErc20';
 import { localVue, i18n, createRouter } from '@/helpers/SetupLocalVue';
 import { createMocks as createStoreMocks } from '@/store/__mocks__/store.js';
 import Wallet from '@/store/wallet/entities/wallet';
 import Coin from '@/store/wallet/entities/coin';
 import cordovaMocks from '~/test/CordovaMocks';
+
 
 const walletData = JSON.parse('{"$id":3,"id":3,"account_id":1,"name":"Ethereum","displayName":"Ethereum","symbol":"ETH","sdk":"Ethereum","network":"ETHEREUM","internalChainAddressIndex":0,"externalChainAddressIndex":1,"externalAddress":null,"confirmedBalance":10,"unconfirmedBalance":0,"imported":true,"enabled":true,"lastBlockHeight":0,"parentName":"","parentSdk":"","contractAddress":"","decimals":"","hdWallet":{"bip":49,"ext":{"xpriv":"xprv9zZZrtcYB6bg2sxhoVYYKAM8duZHP1MwV1SYNVLStU6qpY6NCKfuTCeeHe1MsBeYDt7XipqZYtshoGC9ghNZ6EVwugKxUPM7m2tFmExu2mt","xpub":"xpub6DYvGQ9S1U9yFN3AuX5YgJHsBwPmnU5nrEN9Ask4SodphLRWjrz9zzy88v6HFPGJuDoMg7hhEG2yFFhU7Djqx3AoE57hEL748iFp26JLXtC"},"int":{"xpriv":"xprv9zZZrtcYB6bg6LXLr8kweVuqCWUTc5dnFDJ526bEyz5pLHdfHRx2D1UPLE8bZy6hK8f1YpXQHeGF2FWEds5S1MSbTH2boaQRNUoz6vy4MRq","xpub":"xpub6DYvGQ9S1U9yJpboxAHx1drZkYJx1YMdcSDfpUzrYKcoD5xopyGGkonsBXbmCnHteYYMcjgWRU7EyByaGf8Mav4y5BTkYmC2TD2JVPmkJxs"},"type":1,"network":{"name":"BITCOIN_TESTNET","type":"testnet","bip":1,"segwit":true,"discovery":"http://92.207.178.198:3001/api","broadcastUrl":"https://chain.so/api/v2/send_tx/BTCTEST","feeApi":"https://api.blockcypher.com/v1/btc/main","connect":{"messagePrefix":"Bitcoin Signed Message:","bech32":"tb","bip32":{"public":70617039,"private":70615956},"pubKeyHash":111,"scriptHash":196,"wif":239}}},"erc20Wallet":""}');
 const erc20WalletData = JSON.parse('{"$id":4,"id":4,"account_id":1,"name":"Catalyst","displayName":"Catalyst","symbol":"CAT","sdk":"ERC20","network":"ETHEREUM","internalChainAddressIndex":0,"externalChainAddressIndex":1,"externalAddress":null,"confirmedBalance":10,"unconfirmedBalance":0,"imported":true,"enabled":true,"lastBlockHeight":0,"parentName":"Ethereum","parentSdk":"Ethereum","contractAddress":"123","decimals":"3","hdWallet":{"bip":49,"ext":{"xpriv":"xprv9zZZrtcYB6bg2sxhoVYYKAM8duZHP1MwV1SYNVLStU6qpY6NCKfuTCeeHe1MsBeYDt7XipqZYtshoGC9ghNZ6EVwugKxUPM7m2tFmExu2mt","xpub":"xpub6DYvGQ9S1U9yFN3AuX5YgJHsBwPmnU5nrEN9Ask4SodphLRWjrz9zzy88v6HFPGJuDoMg7hhEG2yFFhU7Djqx3AoE57hEL748iFp26JLXtC"},"int":{"xpriv":"xprv9zZZrtcYB6bg6LXLr8kweVuqCWUTc5dnFDJ526bEyz5pLHdfHRx2D1UPLE8bZy6hK8f1YpXQHeGF2FWEds5S1MSbTH2boaQRNUoz6vy4MRq","xpub":"xpub6DYvGQ9S1U9yJpboxAHx1drZkYJx1YMdcSDfpUzrYKcoD5xopyGGkonsBXbmCnHteYYMcjgWRU7EyByaGf8Mav4y5BTkYmC2TD2JVPmkJxs"},"type":1,"network":{"name":"BITCOIN_TESTNET","type":"testnet","bip":1,"segwit":true,"discovery":"http://92.207.178.198:3001/api","broadcastUrl":"https://chain.so/api/v2/send_tx/BTCTEST","feeApi":"https://api.blockcypher.com/v1/btc/main","connect":{"messagePrefix":"Bitcoin Signed Message:","bech32":"tb","bip32":{"public":70617039,"private":70615956},"pubKeyHash":111,"scriptHash":196,"wif":239}}},"erc20Wallet":""}');
@@ -48,6 +50,7 @@ describe('AddERC20 component', () => {
       propsData,
       store: storeMocks.store,
       attachToDocument: true,
+      parentComponent: AddErc20Modal,
       mocks: {
         coinSDKS: coinSDKSMock,
         errorHandler: jest.fn(),
@@ -103,7 +106,7 @@ describe('AddERC20 component', () => {
           expect(storeMocks.actions.setAddErc20ModalOpened).toHaveBeenCalled();
           expect(storeMocks.actions.setAddErc20ModalOpened.mock.calls[0][1]).toBe(false);
           done();
-        }, 700);
+        }, 1000);
       });
 
       it('passes the error to the errorHandler if QRScanner fails', async (done) => {
@@ -342,6 +345,15 @@ describe('AddERC20 component', () => {
           done();
         }, 100);
       }, 100);
+    });
+
+    describe('parent modal', () => {
+      it('opens and closes the modal', () => {
+        wrapper.vm.$parent.$store = wrapper.vm.$store;
+        wrapper.vm.$parent.addErc20ModalOpened = false;
+        expect(wrapper.vm.$parent.addErc20ModalOpened).toBe(false);
+        expect(storeMocks.actions.setAddErc20ModalOpened).toHaveBeenCalled();
+      });
     });
   });
 });
