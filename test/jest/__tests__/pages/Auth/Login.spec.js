48fb0c1e8f534fd6e309fded5e36a326700b7424
Stephen <stephen.horsfall@atlascity.io>
2019-06-10 19:23:58 +0100

    315 as a developer i want the pin pad clear button to reset the pin state on delete account (#316)
    * reset pin state on delete account pinpad clear button
    
    * display error toast and clear pin state on invalid pin
    
    * clear state on invalid pin input on login
    
    * update deleteAccount unit tests
    
    * update Login unit tests
    
    * remove duplicates
    
    * fix identation

--- test/jest/__tests__/pages/Auth/Login.spec.js
+++ test/jest/__tests__/pages/Auth/Login.spec.js
@@ -137,9 +137,19 @@ describe('Login.vue', () => {
   });
 
   it('displays a wrong pin toast if pin doesn\'t match', () => {
+    const mockResetPin = jest.fn();
+    wrapper.setMethods({ resetPin: mockResetPin });
+
+    const mockResetState = jest.fn();
+    wrapper.vm.$refs.PinPad = {
+      resetState: mockResetState,
+    };
+
     wrapper.vm.pin = [0, 0, 0, 0, 0, 1];
     wrapper.vm.attemptUnlock();
     expect(wrapper.vm.$toast.create).toHaveBeenCalled();
+    expect(mockResetState).toHaveBeenCalled();
+    expect(mockResetPin).toHaveBeenCalled();
   });
 
   it('catches errors', (done) => {
