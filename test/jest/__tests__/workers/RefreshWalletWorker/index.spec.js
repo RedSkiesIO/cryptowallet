c1c53643a5bc26529aeb5b1efe9a25b9d080c2db
stephenhorsfall <stephen.horsfall@atlascity.io>
2019-07-26 16:31:59 +0100

    add unit tests

--- test/jest/__tests__/workers/RefreshWalletWorker/index.spec.js
+++ test/jest/__tests__/workers/RefreshWalletWorker/index.spec.js
@@ -1,8 +1,28 @@
 import RefreshWalletWorker from '@/workers/RefreshWallet';
+import Wallet from '@/store/wallet/entities/wallet';
+// import Tx from '@/store/wallet/entities/tx';
 
 describe('RefreshWallet worker', () => {
   it('can spawn a refresh wallet worker', async () => {
+    Wallet.$update = jest.fn();
     const instance = await new RefreshWalletWorker();
-    console.log(await instance.refreshWallet());
+    const cb = await instance.refreshWallet();
+    cb.funcs.updateWallet(1, { newBalance: 5 });
+    cb.funcs.updateWallet(1, { addr: '123' });
   });
+
+  it('can update a wallet', async () => {
+    Wallet.$update = jest.fn();
+    const instance = await new RefreshWalletWorker();
+    const cb = await instance.refreshWallet();
+    cb.funcs.updateWallet(1, { newBalance: 5 });
+    cb.funcs.updateWallet(1, { addr: '123' });
+  });
+
+//   it('can get txs', async () => {
+//     Tx.query = jest.fn();
+//     const instance = await new RefreshWalletWorker();
+//     const cb = await instance.refreshWallet();
+//     cb.funcs.getTxs(1);
+//   });
 });
