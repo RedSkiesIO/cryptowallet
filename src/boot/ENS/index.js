aedc0d6bf148c44a9e15190b042d0ce3a40acbeb
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2020-05-27 15:14:03 +0100

    add ens plugin

---
+++ src/boot/ENS/index.js
@@ -0,0 +1,26 @@
+// import ENS from 'ethereum-ens';
+// import Web3 from 'web3';
+import { ethers } from 'ethers';
+import networks from '@/store/settings/state/supportedNetworks';
+
+export class ENSResolver {
+  constructor(network) {
+    const url = networks[network].provider;
+    this.provider = new ethers.providers.JsonRpcProvider(url);
+    // this.ens = new ENS(this.provider);
+  }
+
+  async resolver(name) {
+    const address = await this.provider.resolveName(name);
+    return address;
+  }
+
+  async lookup(address) {
+    const name = await this.provider.lookupAddress(address);
+    return name;
+  }
+}
+
+export default ({ Vue }) => {
+  Vue.prototype.$ens = ENSResolver;
+};
