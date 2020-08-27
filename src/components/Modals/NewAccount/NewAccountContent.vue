267d40ded55d71b1067fe70b4d31f99f8d18ef99
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2020-08-27 14:56:28 +0100

    change account recovery layout and move seed phrase option to splash screen

--- src/components/Modals/NewAccount/NewAccountContent.vue
+++ src/components/Modals/NewAccount/NewAccountContent.vue
@@ -25,15 +25,32 @@
           @click="createAccount"
         />
       </div>
-      <div class="btns-wrapper">
-        <q-btn
-          flat
-          :label="$t('importAccount')"
-          icon="get_app"
-          color="primary"
-          text-color="primary"
-          @click="importAccount"
-        />
+      <div class="advanced-dropdown q-mt-sm">
+        <q-expansion-item
+          label="Advanced"
+          color="info"
+        >
+          <q-btn
+            :label="$t('seedPhraseCreate')"
+            flat
+            no-caps
+            dense
+            class="import-account-btn"
+            text-color="blue"
+            size="13px"
+            @click="seedPhrase()"
+          />
+          <q-btn
+            :label="$t('seedPhraseImport')"
+            flat
+            no-caps
+            dense
+            class="import-account-btn"
+            text-color="blue"
+            size="13px"
+            @click="importAccount()"
+          />
+        </q-expansion-item>
       </div>
     </div>
   </div>
@@ -58,10 +75,37 @@ export default {
       this.$store.dispatch('setup/setAccountType', 'restored');
       this.$router.push({ path: '/setup/1' });
     },
+
+    seedPhrase() {
+      this.$store.dispatch('setup/setAccountLocale', this.selectedLang.value);
+      this.$store.dispatch('setup/setAccountCurrency', this.$t('supportedCurrency'));
+      this.$store.dispatch('setup/setAccountType', 'new');
+      this.$router.push({ path: '/setup/2' });
+    },
   },
 };
 </script>
 
 <style>
+.import-account-btn {
+  width: 15rem;
+}
+.advanced-dropdown .q-expansion-item__container {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%);
+}
 
+.advanced-dropdown .q-item {
+  justify-content: center;
+  min-height: 0px;
+}
+
+.advanced-dropdown .q-item__section--main {
+  flex: none;
+}
+
+.advanced-dropdown .q-item__section--main ~ .q-item__section--side {
+  padding-left: 2px;
+}
 </style>
