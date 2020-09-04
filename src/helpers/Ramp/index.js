4843664a111672ac996d03a1aa9b68f5fff36f22
Stephen Horsfall <991266+stxphxn@users.noreply.github.com>
2020-09-04 13:56:22 +0100

    enable payment staging environment

--- src/helpers/Ramp/index.js
+++ src/helpers/Ramp/index.js
@@ -32,13 +32,16 @@ export class Ramp {
     this.config = {
       hostAppName: 'Cent',
       hostLogoUrl: 'https://ramp-website.netlify.app/assets/images/Logo.svg',
-      hostApiKey: 'bbt5vhccp62ryb92zfgetrdtwtwqdx78e28qap4m',
       // swapAsset: wallet.symbol,
       userAddress: wallet.externalAddress,
       userEmailAddress: account.email || '',
       variant: 'mobile',
     };
-    if (testnet) { this.config.url = 'https://ri-widget-staging.firebaseapp.com/'; }
+    if (testnet) {
+      this.config.url = 'https://ri-widget-staging.firebaseapp.com/';
+    } else {
+      this.config.hostApiKey = 'bbt5vhccp62ryb92zfgetrdtwtwqdx78e28qap4m';
+    }
   }
 
   open() {
